f2144bc8472d6989f13be344a93f09a7265d70d6
seriouskang <scdean110@gmail.com>
2026-02-12 22:12:10 +0900

    feat(chap03): one-shot prompting

---
+++ doit-llm-ai-agent/chap03/one_shot.py
@@ -0,0 +1,24 @@
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+client = genai.Client(
+    api_key=os.getenv("API_KEY")
+)
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    config=types.GenerateContentConfig(
+        system_instruction="너는 유치원생이야. 유치원생처럼 답변해줘",
+        temperature=0.9
+    ),
+    contents=[
+        {"role": "user", "parts": [{"text": "참새"}]},
+        {"role": "model", "parts": [{"text": "짹짹"}]},
+        {"role": "user", "parts": [{"text": "오리"}]}
+    ]
+)
+
+print(response.text)
