5b366b9ab627882a3810b782a196934b22b25604
seriouskang <scdean110@gmail.com>
2026-02-16 18:04:10 +0900

    refactor(chap03): streamlit basic 개선

--- doit-llm-ai-agent/chap03/streamlit_basic.py
+++ doit-llm-ai-agent/chap03/streamlit_basic.py
@@ -1,9 +1,9 @@
 import streamlit as st
+import os
 import time
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 client = genai.Client(
@@ -12,9 +12,11 @@ client = genai.Client(
 
 def get_ai_response(messages):
     return client.models.generate_content(
-        model="gemini-2.5-flash",
+        model="gemini-2.5-flash-lite",
         contents=st.session_state.messages
     ).text
+
+st.title("Chatbot 🤖")
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -22,13 +24,27 @@ if "messages" not in st.session_state:
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
-    st.chat_message(message["role"]).write(message["parts"][0]["text"])
+    with st.chat_message(message["role"]):
+        st.markdown(message["parts"][0]["text"])
 
 # Accept user input
 if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "parts": [{"text": prompt}]})    
-    st.chat_message("user").write(prompt)
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-    response = get_ai_response(st.session_state.messages)
+    response = get_ai_response(st.session_state.messages)        
+    with st.chat_message("model"):
+        message_placeholder = st.empty()
+        full_response = ""
+
+        # Simulate stream of response with milliseconds delay
+        for chunk in response.split():
+            full_response += chunk + " "
+            time.sleep(0.03)
+            # Add a blinking cursor to simulate typing
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    
+    # Add assistant response to chat history
     st.session_state.messages.append({"role": "model", "parts": [{"text": response}]})
-    st.chat_message("model").write(response)
